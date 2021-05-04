0e6894b8762b81a3b63b13fefd8ae70b38090235
AkhundSheeraz <akhund.sheeraz@gmail.com>
2021-05-04 16:27:36 +0500

    markup forgetpass added with css

--- forgetpass.php
+++ forgetpass.php
@@ -1,10 +1,26 @@
 <?php
-class forgotpass
-{
-    public $getmail;
+require './connection.php';
+?>
 
-    function __construct($getmail)
-    {
-        $this->mail = $getmail;
-    }
-}
+<html lang="en">
+
+<?php include './views/header.php' ?>
+
+<?php include './views/navbar.php' ?>
+
+<body>
+
+    <div class="containfp">
+        <form class="forgetpass" id="resetcode">
+            <img class="invert" src="./img/userblack.png" alt="user">
+            <h3 class="text-center text-white">Username</h3>
+            <input class="frminputs" type="password" name="recode" id="pass" placeholder="New password">
+            <input class="frminputs" type="password" name="recode2" id="pass1" placeholder="Confirm-password">
+            <button class="btn btn-success py-0 w-50" type="submit">Change Password</button>
+        </form>
+    </div>
+
+    <?php include './views/footer.php' ?>
+</body>
+
+</html>
